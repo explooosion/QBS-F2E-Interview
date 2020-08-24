0e5aa848d8db02f0d0605ef3a70472769021eb5d
explooosion <ta7382@gmail.com>
2020-08-24 03:01:15 +0000

    Deploying to gh-pages from master 337bfc341dc8d076cf3a7ec455f627cadb04286b

---
+++ precache-manifest.7c8e4a03c9909f7c7e4551adb6a7e985.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d6e20c2c3c57e153b21405f0c7477a1",
+    "url": "/QBS-F2E-Interview/index.html"
+  },
+  {
+    "revision": "506cca2c15e1e0a69aeb",
+    "url": "/QBS-F2E-Interview/static/css/2.8fe7522e.chunk.css"
+  },
+  {
+    "revision": "c794ef4e954b8774cb0e",
+    "url": "/QBS-F2E-Interview/static/css/main.ccd4d8be.chunk.css"
+  },
+  {
+    "revision": "506cca2c15e1e0a69aeb",
+    "url": "/QBS-F2E-Interview/static/js/2.42a2d28b.chunk.js"
+  },
+  {
+    "revision": "b3e8ebebd2bed1b75d26cc95d0b56640",
+    "url": "/QBS-F2E-Interview/static/js/2.42a2d28b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c794ef4e954b8774cb0e",
+    "url": "/QBS-F2E-Interview/static/js/main.3f10932d.chunk.js"
+  },
+  {
+    "revision": "c497a119a56caa20b849",
+    "url": "/QBS-F2E-Interview/static/js/runtime-main.ce145c53.js"
+  },
+  {
+    "revision": "4ab6d405749d83249426e48b0ebf3c91",
+    "url": "/QBS-F2E-Interview/static/media/alipay.4ab6d405.svg"
+  },
+  {
+    "revision": "17be717f1cf803e19318dccfc664e084",
+    "url": "/QBS-F2E-Interview/static/media/mastercard-old.17be717f.svg"
+  },
+  {
+    "revision": "4bc44e56d175c2de770df332e9fc3fd6",
+    "url": "/QBS-F2E-Interview/static/media/paypal.4bc44e56.svg"
+  },
+  {
+    "revision": "e80816ad167925a213a6fa9a82d774a2",
+    "url": "/QBS-F2E-Interview/static/media/unionpay.e80816ad.svg"
+  },
+  {
+    "revision": "14c638ba4161e87a8477cd2ed2faadd2",
+    "url": "/QBS-F2E-Interview/static/media/verve.14c638ba.svg"
+  },
+  {
+    "revision": "bca381c0504cc89620c47279f2d7c8d4",
+    "url": "/QBS-F2E-Interview/static/media/visa.bca381c0.svg"
+  }
+]);
